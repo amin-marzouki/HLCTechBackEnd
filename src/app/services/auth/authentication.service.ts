9232edb1aa6d42a9b4fcdecc78a3643f51da4d6b
amine <amine.marzouqui@gmail.com>
2024-07-12 14:01:27 +0100

    isncription + payment

---
+++ src/app/services/auth/authentication.service.ts
@@ -0,0 +1,103 @@
+import {ApiConfiguration} from "../ApiConfiguration";
+import {register, Register$Params} from "../fn/auth/register";
+import {HttpClient, HttpContext} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {StrictHttpResponse} from "../StrictHttpResponse";
+import {map} from "rxjs/operators";
+import {authenticate, Authenticate$Params} from "../fn/auth/authenticate";
+import {AuthenticationResponse} from "../../models/auth/AuthenticationResponse";
+import {BaseService} from "./base.service";
+import {Injectable} from "@angular/core";
+import {Confirm$Params} from "../fn/auth/Confirm$Params";
+import {confirm} from "./confirm";
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationService extends BaseService{
+
+  constructor(config: ApiConfiguration, http: HttpClient) {
+    super(config, http);
+  }
+
+  /** Path part for operation `register()` */
+  static readonly RegisterPath = '/auth/register';
+
+  /**
+   * This method provides access to the full `HttpResponse`, allowing access to response headers.
+   * To access only the response body, use `register()` instead.
+   *
+   * This method sends `application/json` and handles request body of type `application/json`.
+   */
+  register$Response(params: Register$Params, context?: HttpContext): Observable<StrictHttpResponse<{
+  }>> {
+    return register(this.http, this.rootUrl, params, context);
+  }
+
+  /**
+   * This method provides access only to the response body.
+   * To access the full response (for headers, for example), `register$Response()` instead.
+   *
+   * This method sends `application/json` and handles request body of type `application/json`.
+   */
+  register(params: Register$Params, context?: HttpContext): Observable<{
+  }> {
+    return this.register$Response(params, context).pipe(
+      map((r: StrictHttpResponse<{
+      }>): {
+      } => r.body)
+    );
+  }
+
+  /** Path part for operation `authenticate()` */
+  static readonly AuthenticatePath = '/auth/authenticate';
+
+  /**
+   * This method provides access to the full `HttpResponse`, allowing access to response headers.
+   * To access only the response body, use `authenticate()` instead.
+   *
+   * This method sends `application/json` and handles request body of type `application/json`.
+   */
+  authenticate$Response(params: Authenticate$Params, context?: HttpContext): Observable<StrictHttpResponse<AuthenticationResponse>> {
+    return authenticate(this.http, this.rootUrl, params, context);
+  }
+
+  /**
+   * This method provides access only to the response body.
+   * To access the full response (for headers, for example), `authenticate$Response()` instead.
+   *
+   * This method sends `application/json` and handles request body of type `application/json`.
+   */
+  authenticate(params: Authenticate$Params, context?: HttpContext): Observable<AuthenticationResponse> {
+    return this.authenticate$Response(params, context).pipe(
+      map((r: StrictHttpResponse<AuthenticationResponse>): AuthenticationResponse => r.body)
+    );
+  }
+
+  /** Path part for operation `confirm()` */
+  static readonly ConfirmPath = '/auth/activate-account';
+
+  /**
+   * This method provides access to the full `HttpResponse`, allowing access to response headers.
+   * To access only the response body, use `confirm()` instead.
+   *
+   * This method doesn't expect any request body.
+   */
+  confirm$Response(params: Confirm$Params, context?: HttpContext): Observable<StrictHttpResponse<void>> {
+    return confirm(this.http, this.rootUrl, params, context);
+  }
+
+  /**
+   * This method provides access only to the response body.
+   * To access the full response (for headers, for example), `confirm$Response()` instead.
+   *
+   * This method doesn't expect any request body.
+   */
+  confirm(params: Confirm$Params, context?: HttpContext): Observable<void> {
+    return this.confirm$Response(params, context).pipe(
+      map((r: StrictHttpResponse<void>): void => r.body)
+    );
+  }
+}
